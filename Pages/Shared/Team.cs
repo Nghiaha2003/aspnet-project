d8df08652f8f9df352bab053cb8631ff3cfd76e4
duongle01555 <duongle01555@gmail.com>
2024-08-01 17:55:33 +1000

    duong test 1

---
+++ Pages/Shared/Team.cs
@@ -0,0 +1,6 @@
+﻿namespace League.Pages.Shared
+{
+    public class Team
+    {
+    }
+}
---
+++ Pages/Team.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace League.Pages
+{
+    public class TeamModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
