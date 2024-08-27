f0fbbc904e2e2756e92b3ce29b858e8b85f9b865
duongle01555 <duongle01555@gmail.com>
2024-08-27 16:41:50 +1000

    add calculated field

--- Pages/Team/TeamDetail.cshtml.cs
+++ Pages/Team/TeamDetail.cshtml.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace League.Pages.Team
 {
@@ -12,19 +13,33 @@ namespace League.Pages.Team
     {
         private readonly LeagueContext _context;
 
-        [BindProperty]
-        public string Id { get; set; }
         public TeamDetailModel(LeagueContext context)
         {
             _context = context;
         }
         public Models.Team Team { get; set; }
+        public Models.TeamStats TeamStat { get; set; }
+
+        public LeagueContext Get_context()
+        {
+            return _context;
+        }
+
         public async Task<IActionResult> OnGetAsync(string Id)
         {
             Team = await _context.Teams
                 .Include(c => c.Players)
                 .AsNoTracking()
                 .FirstOrDefaultAsync(m => m.TeamId == Id);
+
+
+            var teamstat = from b in _context.Teams
+                           where b.TeamId == Id
+                           select new TeamStats
+                           {
+                               WinLossRatio = b.Win / b.Loss
+                           };
+            TeamStat = await teamstat.SingleOrDefaultAsync();
             return Page();
         }
     }
